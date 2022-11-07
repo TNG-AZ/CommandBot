21b9adf2509542e970007e4e02c8d69601582022
Drake Perrior-Small <drakecsmall@gmail.com>
2022-11-06 23:57:54 -0700

    callback hell
    callback hell

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import time
+
 import discord
 from discord.ext import commands
 from discord.ui import Select, View
 from discord import SelectOption
 from datetime import datetime
+
+
 #from config_example import TOKEN
 from config import TOKEN
+
 
 async def get_future_event_selectmenu(ctx: discord.ApplicationContext):
     events = await ctx.guild.fetch_scheduled_events()
@@ -14,7 +19,6 @@ async def get_future_event_selectmenu(ctx: discord.ApplicationContext):
     )
     options = []
     for event in events:
-        print(event.name)
         options.append(SelectOption(
             label=f"{event.name} on {event.start_time:%d-%m-%y}",
             value=str(event.id),
@@ -26,20 +30,56 @@ async def get_future_event_selectmenu(ctx: discord.ApplicationContext):
         max_values=1,
         options=options
     )
-
     return select
 
 
 intents = discord.Intents.none()
 intents.scheduled_events = True
-bot = discord.Bot(intents=intents)
-
+bot = discord.Bot(
+    intents=intents,
+)
 
 @bot.slash_command(name="eventdm")
 async def event_dm(ctx: discord.ApplicationContext):
-    options = await get_future_event_selectmenu(ctx)
-    view = View(options)
-    await ctx.send("Choose an event", view=view)
+    event_options = await get_future_event_selectmenu(ctx)
+
+    message_options = Select(
+        placeholder="How would you like to deliver the message?",
+        min_values=1,
+        max_values=1,
+        options=[
+            SelectOption(label="DM"),
+            SelectOption(label="Tagged message")
+        ]
+    )
+
+    async def event_select_callback(interaction: discord.Interaction):
+        event_id = event_options.values[0]
+
+        async def message_option_callback(interaction: discord.Interaction):
+            nonlocal event_id, event_options
+
+            message_type = message_options.values[0]
+
+            modal = discord.ui.Modal(title="RSVP Mailer")
+            modal.add_item(discord.ui.InputText(label="Message to send", style=discord.InputTextStyle.long))
+
+            async def modal_callback(interaction: discord.Interaction):
+                nonlocal event_id, message_type, modal
+                await ctx.send(f"event:{event_id}, mode:{message_type}, message:{modal.children[0].value}")
+                return await interaction.message.delete()
+
+            modal.callback = modal_callback
+            return await interaction.response.send_modal(modal)
+
+        message_options.callback = message_option_callback
+
+        view = View(message_options)
+        return await interaction.message.edit(view=view)
+
+    event_options.callback = event_select_callback
+    view = View(event_options)
+    await ctx.respond("", view=view)
 
 
 bot.run(TOKEN)
