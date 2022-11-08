e8fff6d54742889cbd1a030b5860ccef2b84207e
Drake Perrior-Small <drakecsmall@gmail.com>
2022-11-07 22:55:45 -0700

    Update main.py
    add DM and tagged message generators

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import time
 
 import discord
 from discord.ext import commands
-from discord.ui import Select, View
+from discord.ui import Select, View, Button
 from discord import SelectOption
 from datetime import datetime
 
-
-#from config_example import TOKEN
+# from config_example import TOKEN
 from config import TOKEN
 
 
 async def get_future_event_selectmenu(ctx: discord.ApplicationContext):
     events = await ctx.guild.fetch_scheduled_events()
+    if not events:
+        return
+
     events = sorted(
         [e for e in events if e.start_time.date() >= datetime.utcnow().date()],
         key=lambda e: e.start_time.timestamp
@@ -39,9 +41,13 @@ bot = discord.Bot(
     intents=intents,
 )
 
+
 @bot.slash_command(name="eventdm")
 async def event_dm(ctx: discord.ApplicationContext):
     event_options = await get_future_event_selectmenu(ctx)
+
+    if not event_options:
+        return await ctx.send("no events coming up")
 
     message_options = Select(
         placeholder="How would you like to deliver the message?",
@@ -54,7 +60,17 @@ async def event_dm(ctx: discord.ApplicationContext):
     )
 
     async def event_select_callback(interaction: discord.Interaction):
+        event_options.disabled = True
         event_id = event_options.values[0]
+        event = await ctx.guild.fetch_scheduled_event(event_id)
+
+        if event.subscriber_count == 0:
+            return await interaction.response.send_message(f"No one seems to be interested in {event.name}")
+
+        if event.subscriber_count == 1:
+            subscribers = await event.subscribers().next()
+        else:
+            subscribers = await event.subscribers().flatten()
 
         async def message_option_callback(interaction: discord.Interaction):
             nonlocal event_id, event_options
@@ -65,17 +81,57 @@ async def event_dm(ctx: discord.ApplicationContext):
             modal.add_item(discord.ui.InputText(label="Message to send", style=discord.InputTextStyle.long))
 
             async def modal_callback(interaction: discord.Interaction):
-                nonlocal event_id, message_type, modal
-                await ctx.send(f"event:{event_id}, mode:{message_type}, message:{modal.children[0].value}")
-                return await interaction.message.delete()
+                nonlocal event_id, message_type, subscribers, modal
+                modal.stop()
+
+                message = modal.children[0].value
+
+                if event.subscriber_count == 1:
+                    target_names = subscribers.name
+                else:
+                    target_names = ", ".join([s.name for s in subscribers])
+
+                await interaction.response.send_message(
+                    f"""event:{event.name} on {event.start_time:%d-%m-%y}
+mode:{message_type}, to:{target_names}
+message:{message}"""
+                )
+
+                confirm_button = Button(
+                    label="Send Message"
+                )
+
+                async def confirm_callback(interaction: discord.Interaction):
+                    nonlocal subscribers, message
+                    await interaction.message.delete()
+                    if event.subscriber_count == 1:
+                        subscribers = [subscribers]
+
+                    if message_type == "DM":
+                        for sub in subscribers:
+                            await sub.send(message)
+                        await ctx.send(view=View(Button(disabled=True, label="SENT")))
+                    else:
+                        message_with_tag = ", ".join([sub.mention for sub in subscribers])
+                        message_with_tag += "\n\n"
+                        message_with_tag += message
+                        await ctx.send(message_with_tag)
+                    return await interaction.response.defer()
+
+                confirm_button.callback = confirm_callback
+                view = View(confirm_button)
+                await ctx.send(view=view)
+                return await interaction.response.defer()
 
             modal.callback = modal_callback
-            return await interaction.response.send_modal(modal)
+            await interaction.response.send_modal(modal)
+            return await interaction.message.delete()
 
         message_options.callback = message_option_callback
 
         view = View(message_options)
-        return await interaction.message.edit(view=view)
+        await interaction.message.edit(view=view)
+        return await interaction.response.defer()
 
     event_options.callback = event_select_callback
     view = View(event_options)
@@ -83,4 +139,3 @@ async def event_dm(ctx: discord.ApplicationContext):
 
 
 bot.run(TOKEN)
-
